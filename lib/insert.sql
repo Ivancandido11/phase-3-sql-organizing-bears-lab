a26e8d4d0884acc7b8b07b32c2d3993e23bb73b1
Ivan Candido <ivan.candido@powerhrg.com>
2021-08-18 08:55:44 -0400

    Completed Assignment

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Mr. Chocolate", 11, "M", "Brown", "Calm", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Rowdy", 3, "F", "Black", "Aggressive", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Tabitha", 6, "F", "Black", "Aggressive", 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Segeant Brown", 7, "M", "Brown", "Playful", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Melissa", 10, "F", "Brown", "Calm", 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Grinch", 2, "M", "Black", "Aggressive", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Wendy", 5, "F", "Black", "Playful", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, 9, "M", "Brown", "Aggressive", 0);
